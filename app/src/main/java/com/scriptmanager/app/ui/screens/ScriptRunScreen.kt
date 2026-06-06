8d77b40b364752334474aeb1bb621422aa9e6179
RuSheng920 <1393824573@qq.com>
2026-06-07 03:05:23 +0800

    Fix missing imports + add SDK 37 to CI

--- app/src/main/java/com/scriptmanager/app/ui/screens/ScriptRunScreen.kt
+++ app/src/main/java/com/scriptmanager/app/ui/screens/ScriptRunScreen.kt
@@ -1,9 +1,11 @@
 package com.scriptmanager.app.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
